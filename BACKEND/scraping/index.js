5fdc2bd8150e0b4845bc15529ef35cbcdf63dcd9
stopcagic <49293128+Saud-T@users.noreply.github.com>
2020-05-05 14:46:15 +0200

    Update index.js

--- BACKEND/scraping/index.js
+++ BACKEND/scraping/index.js
@@ -1,2 +1,10 @@
 import axios from 'axios'
 import cheerio from 'cheerio'
+
+
+const url = 'https://fipu.unipu.hr/fipu/o_fakultetu/nastavnici'
+
+let data = async () => {
+    let html = await axios.get(url)
+    let $ = cheerio.load(html.data)
+}
