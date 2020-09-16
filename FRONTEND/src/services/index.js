18d35a5ff2856dcebfc9e64086742e4414d82698
stopcagic <49293128+stopcagic@users.noreply.github.com>
2020-09-16 21:47:55 +0200

    push

--- FRONTEND/src/services/index.js
+++ FRONTEND/src/services/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import router from "@/router";
 
 let Services = axios.create({
-  baseURL: "https://still-castle-86560.herokuapp.com/",
+  baseURL: "http://localhost:3000/",
   timeout: 10000,
 });
 
